44b93725cc27dd4aad22549b68dfed78fa234a96
vladislav kazak <vladik.zakaz@gmail.com>
2020-10-15 15:19:17 +0300

    Выполнено усложненное задание

--- script/script.js
+++ script/script.js
@@ -1,14 +1,21 @@
-const computerNumber = 6;
 let userNumber; 
-//userNumber=prompt("Угадай число от 1 до 100");
 
-//console.log (isNaN(userNumber));
-
+let counter = 10;
+inputNumber();
 function inputNumber(){
+    let computerNumber = 6;
+    function newGame () {
+        if(confirm("Хотели бы сыграть еще раз?")){
+            counter = 10;
+            inputNumber();
+         }          
+         return;
+    };
     userNumber=prompt("Угадай число от 1 до 100");
     
     if (userNumber===null){
          alert("Игра окончена");
+         
          return;
         }
     if(isNaN(userNumber)) {      
@@ -17,15 +24,24 @@ function inputNumber(){
     }
     if (userNumber==computerNumber) {
         alert("Вы угадали");
+       newGame();
+         
     }
-    if (userNumber>computerNumber) {
-        alert("Загаданное число меньше");
-        inputNumber();
-    }
-    if ((userNumber>0)&&(userNumber<computerNumber)) {
-        alert("Загаданное число больше");
-        inputNumber();
-    }
+    if (counter == 0) {
+        alert("Попытки закончились");
+        newGame();}
+    if (counter != 0) {
+     
     
+        if (userNumber>computerNumber) {
+         alert("Загаданное число меньше, осталось "+ --counter +" Попыток");
+         inputNumber();
+            
+    }
+        if ((userNumber>0)&&(userNumber<computerNumber)) {
+            alert("Загаданное число больше, осталось "+ --counter +" Попыток");
+            inputNumber();
+    }
 }
-inputNumber();
+}
+
